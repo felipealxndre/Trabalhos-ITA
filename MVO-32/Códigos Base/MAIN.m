092c60d92708fbc96f8ac637f1a259d171b73d73
TatiPasold <tatiana.pasold@insiderstore.com.br>
2025-11-08 13:02:13 -0300

    Update MAIN.m

--- MVO-32/Códigos Base/MAIN.m
+++ MVO-32/Códigos Base/MAIN.m
@@ -18,3 +18,27 @@ options = optimset('Display','iter','TolX',1e-10,'TolFun',1e-10);
 % options = optimoptions(@fsolve,'Display','iter','StepTolerance',1e-10,'FunctionTolerance',1e-10);
 % % or:
 % options = optimoptions(@fsolve,'Display','iter','TolX',1e-10,'TolFun',1e-10);
+
+%% Ex.2
+
+trim_output = struct('X_eq',zeros(6,1),'U_eq',zeros(3,1),...
+    'Y_eq',zeros(8,1));
+
+x_eq_0 = zeros(6,1);
+x_eq_0(1) = trim_par.V;
+x_eq = fsolve(@trim_function,x_eq_0,options,trim_par);
+[~,X_eq,U_eq,Y_eq] = trim_function(x_eq,trim_par);
+
+trim_output.X_eq = X_eq;
+trim_output.U_eq = U_eq;
+trim_output.Y_eq = Y_eq;
+
+%% Ex.3
+
+%% Ex.4
+
+%% Ex.5
+
+%% Ex.6
+
+%% Ex.7
